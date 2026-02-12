e7a52c14637dfddd8effc0fb2e0a0786f4f59424
Sean Ryan <92698057+StrokeOfLuck@users.noreply.github.com>
2026-02-12 17:43:05 -0500

    need to fix showing to many sites

--- main.js
+++ main.js
@@ -482,8 +482,8 @@ sitesDS.clustering.clusterEvent.addEventListener((clusteredEntities, cluster) =>
   cluster.label.show = true;
   
   cluster.label.text = fmtInt(sumPop); // population ONLY
-  //const popLine = `Pop: ${fmtInt(sumPop)}`;
-  //const countLine = showClusterSiteCount ? `\nSites: ${clusteredEntities.length}` : "";
+  const popLine = `Pop: ${fmtInt(sumPop)}`;
+  const countLine = showClusterSiteCount ? `\nSites: ${clusteredEntities.length}` : "";
   //cluster.label.text = `${popLine}${countLine}`;
 
   cluster.label.verticalOrigin = Cesium.VerticalOrigin.CENTER;
