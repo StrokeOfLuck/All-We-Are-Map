4a4ab31c7c51ba878523e9f861b893d0d04800f2
Sean Ryan <92698057+StrokeOfLuck@users.noreply.github.com>
2026-02-11 23:29:39 -0500

    hard zoom range test up higher

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ let autoAdvance = true;
 
 // HARD RULE: no clusters at/under this camera height
 // (You can set this to 2000 if you want the hard cutoff tighter)
-const noClusterAtOrBelowMeters = 5000;
+const noClusterAtOrBelowMeters = 10000;
 
 // “Full strength” merge value when zoomed out
 // Bigger = clusters merge sooner/more (helps prevent “two bubbles side-by-side”)
